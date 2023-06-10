93d1152cd09ede43b054a08dfd73faab869ed1e5
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-10 22:46:43 +0530

    Delete assignment_1/222123031ManishRay directory

--- assignment_1/222123031ManishRay/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tinder_clone/pages/root_app.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootPage(),
-  ));
-}
